2c13e45b2dc55f6f8a82e534109b8e7d8b0edeb6
Bhavya Patidar <bhavyapatidar0101@gmail.com>
2024-07-08 19:33:57 +0530

    Added routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 
 import './App.css'
-
+import { Routes,Route } from 'react-router-dom';
+import HomePage from './Components/HomePage/HomePage';
+import Authentication from './Components/Authentication/Authentication';
 function App() {
 
   return (
     <>
-     
+     <Routes>
+        <Route path="/" element={true?<HomePage/>:<Authentication/>}/>
+     </Routes>
     </>
   )
 }
